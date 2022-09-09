7fda994fd4362d926265a66b2f6e3baad39ef189
James Perry <PjPerry28@gmail.com>
2022-09-09 13:52:52 -0700

    pass test for case differences in words

--- lib/Anagrams_Antigrams.rb
+++ lib/Anagrams_Antigrams.rb
@@ -3,8 +3,8 @@ require ('pry')
 class Anagram
 
   def initialize(word1 , word2)
-    @word1 = word1
-    @word2 = word2
+    @word1 = word1.downcase
+    @word2 = word2.downcase
   end
    def anagram_check()
     if (@word1.chars.sort == @word2.chars.sort)
