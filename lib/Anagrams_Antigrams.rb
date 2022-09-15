6c27a51e17b03ad46c976dfc4082c58865415fb1
James Perry <PjPerry28@gmail.com>
2022-09-14 23:59:58 -0700

    pass test for anagrams with spaces

--- lib/Anagrams_Antigrams.rb
+++ lib/Anagrams_Antigrams.rb
@@ -3,8 +3,8 @@ require ('pry')
 class Anagram
 
   def initialize(word1 , word2)
-    @word1 = word1.downcase.chars.sort
-    @word2 = word2.downcase.chars.sort
+    @word1 = word1.downcase.chars.sort.gsub(/[^A-Za-z0-9]/, '')
+    @word2 = word2.downcase.chars.sort.gsub(/[^A-Za-z0-9]/, '')
   end
    def anagram_check()
     if ( @word1 == @word2 )
